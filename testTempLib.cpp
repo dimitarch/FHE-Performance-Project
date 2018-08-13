3e6b0ba33a6cec320c6d5871a213d20f8ca9f913
Dimitar Chakarov <dimitar_ch@hotmail.com>
2018-08-14 00:01:49 +0300

    Operations

--- testTempLib.cpp
+++ testTempLib.cpp
@@ -1,13 +1,5 @@
-//#include <tfhe/tfhe.h>
-//#include <tfhe/tfhe_io.h>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <cmath>
-
-using namespace std;
+double timeAND = 0.014419389, timeORYN = 0.014402810, timeORNY = 0.014405210, timeXNOR = 0.014414890, timeNOR = 0.014415586, timeXOR = 0.014418391, timeOR = 0.014418821, timeNAND = 0.014417746, timeANDNY = 0.014404012, timeANDYN = 0.014400963; 
+double sigmaAND = 0.000115750, sigmaORYN = 0.000123759, sigmaORNY = 0.000115314, sigmaXNOR = 0.000105690, sigmaNOR = 0.000100818, sigmaXOR = 0.000108985, sigmaOR = 0.000107224, sigmaNAND = 0.000107276, sigmaANDNY = 0.000129178, sigmaANDYN = 0.000111601; 
 
 template <class T>
 class timingBool {
@@ -37,8 +29,9 @@ class timingBool {
             countORNY = a;
             countMUX = a;
         }
-        void print();
-        
+        double timing();
+        double sigma();
+
         T timingAND(T a, T b);
         T timingNAND(T a, T b);
         T timingANDYN(T a, T b);
@@ -54,8 +47,15 @@ class timingBool {
 
 
 template <class T>
-void timingBool<T>::print () {
-    cout<<countAND<<"\n"<<countXOR<<"\n"<<countXNOR<<"\n"<<countMUX<<"\n"; 
+double timingBool<T>::timing () {
+    double operation = countAND * timeAND+ countORYN * timeORYN + countORNY * timeORNY + countXNOR * timeXNOR + countNOR * timeNOR + countXOR * timeXOR + countOR * timeOR + countNAND * timeNAND + countANDNY * timeANDNY + countANDYN * timeANDYN + 2 * countMUX * timeXOR; 
+    return operation;
+}
+
+template <class T>
+double timingBool<T>::sigma () {
+    double sigma = countAND * sigmaAND + countORYN * sigmaORYN + countORNY * sigmaORNY + countXNOR * sigmaXNOR + countNOR * sigmaNOR + countXOR * sigmaXOR + countOR * sigmaOR + countNAND * sigmaNAND + countANDNY * sigmaANDNY + countANDYN * sigmaANDYN + 2 * countMUX * sigmaXOR; 
+    return sigma;
 }
 
 template <class T>
@@ -296,6 +296,6 @@ int main() {
     cin>>a>>b;
     cout<<areEqual(a, b)<<endl;
     cout<<addFHE(a, b)<<endl;
-    timingOperation.print();
+    cout<<timingOperation.timing()<<" "<<timingOperation.sigma()<<endl;
     return 0;
 }

2d2740f6e243406c8922940cbff22613b4f499d0
dimitarch <kotka123@gmail.com>
2018-08-27 14:08:28 +0300

    Min, max, addition and subtraction

--- testTempLib_New_Bootstrapping.cpp
+++ testTempLib_New_Bootstrapping.cpp
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-double timeBoot = (0.014419389 + 0.014402810 + 0.014405210 + 0.014414890 + 0.014415586 + 0.014418391 + 0.014418821 + 0.014417746 + 0.014404012 + 0.014400963) / 10; 
-double sigmaBoot = (0.000115750 + 0.000123759 + 0.000115314 + 0.000105690 + 0.000100818 + 0.000108985 + 0.000107224 + 0.000107276 + 0.000129178 + 0.000111601) / 10; 
+double timeBoot = (0.014419389 + 0.014402810 + 0.014405210 + 0.014414890 + 0.014415586 + 0.014418391 + 0.014418821 + 0.014417746 + 0.014404012 + 0.014400963) / 10;
+double sigmaBoot = (0.000115750 + 0.000123759 + 0.000115314 + 0.000105690 + 0.000100818 + 0.000108985 + 0.000107224 + 0.000107276 + 0.000129178 + 0.000111601) / 10;
 
 template <class T>
 class timingBool {
@@ -38,13 +38,13 @@ class timingBool {
 
 template <class T>
 double timingBool<T>::timing () {
-    double operation = countBoot * timeBoot; 
+    double operation = countBoot * timeBoot;
     return operation;
 }
 
 template <class T>
 double timingBool<T>::sigma () {
-    double sigma = countBoot * sigmaBoot; 
+    double sigma = countBoot * sigmaBoot;
     return sigma;
 }
 
@@ -129,12 +129,13 @@ timingBool <bool> timingOperations(0);
 
 template <class T>
 T andFHE (T a, T b) {
-    T result;
+    T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingAND((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingAND((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -142,12 +143,13 @@ T andFHE (T a, T b) {
 
 template <class T>
 T nandFHE (T a, T b) {
-    T result;
+    T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingNAND((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingNAND((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -155,12 +157,13 @@ T nandFHE (T a, T b) {
 
 template <class T>
 T andynFHE (T a, T b) {
-    T result;
+    T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingANDYN((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingANDYN((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -168,12 +171,13 @@ T andynFHE (T a, T b) {
 
 template <class T>
 T andnyFHE (T a, T b) {
-    T result;
+    T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingANDNY((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingANDNY((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -183,10 +187,11 @@ template <class T>
 T xorFHE (T a, T b) {
     T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingXOR((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingXOR((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -194,12 +199,13 @@ T xorFHE (T a, T b) {
 
 template <class T>
 T xnorFHE (T a, T b) {
-    T result;
+    T result = 0;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingXNOR((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingXNOR((bool) (a & ith), (bool) (b & ith));
 
-        result += temp * (1 << i);
+        result += temp * ith;
     }
 
     return  result;
@@ -207,12 +213,13 @@ T xnorFHE (T a, T b) {
 
 template <class T>
 T orFHE (T a, T b) {
-    T result;
-    
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingOR((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    T result = 0;
 
-        result += temp * (1 << i);
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingOR((bool) (a & ith), (bool) (b & ith));
+
+        result += temp * ith;
     }
 
     return  result;
@@ -220,12 +227,13 @@ T orFHE (T a, T b) {
 
 template <class T>
 T orynFHE (T a, T b) {
-    T result;
-    
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingORYN((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    T result = 0;
 
-        result += temp * (1 << i);
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingORYN((bool) (a & ith), (bool) (b & ith));
+
+        result += temp * ith;
     }
 
     return  result;
@@ -233,12 +241,13 @@ T orynFHE (T a, T b) {
 
 template <class T>
 T ornyFHE (T a, T b) {
-    T result;
-    
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingORNY((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    T result = 0;
 
-        result += temp * (1 << i);
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingORNY((bool) (a & ith), (bool) (b & ith));
+
+        result += temp * ith;
     }
 
     return  result;
@@ -246,88 +255,134 @@ T ornyFHE (T a, T b) {
 
 template <class T>
 T norFHE (T a, T b) {
-    T result;
-    
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--){
-        bool temp = timingOperations.timingNOR((bool) (a & (1 << i)), (bool)(b & (1 << i)));
+    T result = 0;
 
-        result += temp * (1 << i);
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+        bool temp = timingOperations.timingNOR((bool) (a & ith), (bool) (b & ith));
+
+        result += temp * ith;
     }
 
     return  result;
 }
 
 template <class T>
-bool areEqual (T a, T b) {   
+bool areEqualFHE (T a, T b) {
     bool result = true, temp;
 
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--) {
-        temp = timingOperations.timingXNOR((bool) (a & (1 << i)), (bool)(b & (1 << i)));
-        
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++) {
+        long long ith = (long long) (1 << i);
+
+        temp = timingOperations.timingXNOR((bool) (a & ith), (bool) (b & ith));
+
         result = timingOperations.timingAND(result, temp);
     }
 
     return result;
-} 
-
-template <class T>
-T minFHE (T a, T b){
-    bool temp;
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--) {
-        temp = timingOperations.timingXNOR(true, true);
-        temp = timingOperations.timingMUX(true, true, true);
-        temp = timingOperations.timingMUX(true, true, true);
-    }
-
-    return (a<b) ? a : b;
 }
 
 template <class T>
-T maxFHE (T a, T b){
-    bool temp;
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--) {
-        temp = timingOperations.timingXNOR(true, true);
-        temp = timingOperations.timingMUX(true, true, true);
-        temp = timingOperations.timingMUX(true, true, true);
+T minFHE (T a, T b) {
+    T result = 0;
+    bool temp = false;
+
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++) {
+        long long ith = (long long) (1 << i);
+
+        bool tmp = timingOperations.timingXNOR((bool) (a & ith), (bool) (b & ith));
+
+        temp = timingOperations.timingMUX(tmp, temp, (bool) (a & ith));
     }
 
-    return (a>b) ? a : b;
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+
+        result += timingOperations.timingMUX(temp, (bool) (b & ith), (bool) (a & ith)) * ith;
+    }
+
+    return result;
 }
 
 template <class T>
-T addFHE (T a, T b){
-    bool temp;
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--) {
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingAND(true, true);
-        temp = timingOperations.timingAND(true, true);
+T maxFHE (T a, T b) {
+    T result = 0;
+    bool temp = false;
+
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++) {
+        long long ith = (long long) (1 << i);
+
+        bool tmp = timingOperations.timingXNOR((bool) (a & ith), (bool) (b & ith));
+
+        temp = timingOperations.timingMUX(tmp, temp, (bool) (a & ith));
     }
 
-    return  (a + b);
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) (1 << i);
+
+        result += timingOperations.timingMUX(temp, (bool) (a & ith), (bool) (b & ith)) * ith;
+    }
+
+    return result;
+}
+
+template <class T>
+T addFHE (T a, T b) {
+    T result = 0;
+    bool carry = false;
+
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++) {
+        long long ith = (long long) ((long long) 1 << i);
+
+        result += timingOperations.timingXOR(timingOperations.timingXOR((bool) (a & ith), (bool) (b & ith)), carry) * ith;
+
+        bool temp = timingOperations.timingOR(timingOperations.timingAND((bool) (a & ith), (bool) (b & ith)), timingOperations.timingAND((bool) (a & ith), carry));
+        carry = timingOperations.timingOR(timingOperations.timingAND(carry, (bool) (b & ith)), temp);
+    }
+
+    return result;
 }
 
 template <class T>
 T subFHE (T a, T b){
-    bool temp;
-    for(int i = sizeof(T)*CHAR_BIT; i >= 0; i--) {
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingXOR(true, true);
-        temp = timingOperations.timingAND(true, true);
-        temp = timingOperations.timingAND(true, true);
-        temp = timingOperations.timingAND(true, true);
+    T result = 0;
+    bool carry = true;
+
+    T complementB = 0;
+    complementB = ~b;
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++){
+        long long ith = (long long) ((long long) 1 << i);
+
+        result += timingOperations.timingXOR(carry, (bool) (complementB & ith)) * ith;
+        carry = timingOperations.timingAND(carry, (bool) (complementB & ith));
+    }
+    complementB = result;
+
+    result = 0;
+    carry = false;
+    for(int i = 0; i < sizeof(T)*CHAR_BIT; i++) {
+        long long ith = (long long) ((long long) 1 << i);
+
+        result += timingOperations.timingXOR(timingOperations.timingXOR((bool) (a & ith), (bool) (complementB & ith)), carry) * ith;
+
+        bool temp = timingOperations.timingOR(timingOperations.timingAND((bool) (a & ith), (bool) (complementB & ith)), timingOperations.timingAND((bool) (a & ith), carry));
+        carry = timingOperations.timingOR(timingOperations.timingAND(carry, (bool) (complementB & ith)), temp);
     }
 
-    return  (a - b);
+    return result;
+}
+
+void approximateTimeFHE () {
+    cout<<"Expected execution (in seconds): "<<timingOperations.timing()<<"\r\n";
+    cout<<"Expected deviation (in seconds): "<<timingOperations.sigma()<<"\r\n";
 }
 
 int main() {
-    int a, b;
+    long long a, b;
 
     cin>>a>>b;
-    cout<<areEqual(a, b)<<"\r\n";
+    cout<<subFHE(a, b)<<"\r\n";
+
+    approximateTimeFHE();
     return 0;
 }
